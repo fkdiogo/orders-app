10d97e71bce205b0511369e075919252d8aac22e
Felipe F. Diogo <fkdiogo@gmail.com>
2016-12-27 11:37:57 -0200

    adding pending files

--- src/main/java/com/fkdiogo/Application.java
+++ src/main/java/com/fkdiogo/Application.java
@@ -2,13 +2,16 @@ package com.fkdiogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spark.Spark;
 
 /**
  * Created by felipediogo on 09/12/16.
  */
 @SpringBootApplication
 public class Application {
+    private static final int DEFAULT_SPARK_PORT = 4567;
     public static void main(String[] args) {
+        Spark.port()
         SpringApplication.run(Application.class, args);
     }
 }
